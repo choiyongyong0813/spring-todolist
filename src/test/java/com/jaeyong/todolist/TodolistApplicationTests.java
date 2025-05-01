ebf2df54b19c3a3a7fb52c66f31d83cb302634c2
최재용 <choiyongyong0813@gmail.com>
2025-05-01 19:24:39 +0900

    수정

--- src/test/java/com/jaeyong/todolist/TodolistApplicationTests.java
+++ src/test/java/com/jaeyong/todolist/TodolistApplicationTests.java
@@ -1,8 +1,9 @@
 package com.jaeyong.todolist;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+@Disabled("Run 시 테스트 실행 방지")
 @SpringBootTest
 class TodolistApplicationTests {
 
