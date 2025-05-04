4011e9e044f5c1fa31ad20a19e7de2da97a3a7e0
최재용 <choiyongyong0813@gmail.com>
2025-05-04 20:28:03 +0900

    유저 DTO 추가

--- src/main/java/com/jaeyong/todolist/dto/UserUpdateDTO.java
+++ src/main/java/com/jaeyong/todolist/dto/UserUpdateDTO.java
@@ -1,4 +1,14 @@
 package com.jaeyong.todolist.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserUpdateDTO {
+    @NotBlank(message = "닉네임은 필수 입니다!")
+    private String nickname;
+    @NotBlank(message = "비밀번호는 필수 입니다!")
+    private String password;
 }
